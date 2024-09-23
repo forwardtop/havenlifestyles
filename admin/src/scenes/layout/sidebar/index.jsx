f6b81f7613957efe5d221c73ac952da457eb6364
forwardtop <noproblemgod@hotmail.com>
2024-09-23 18:10:11 +0300

    haven admin page

---
+++ admin/src/scenes/layout/sidebar/index.jsx
@@ -0,0 +1,255 @@
+/* eslint-disable react/prop-types */
+import { Avatar, Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useContext, useState } from "react";
+import { tokens } from "../../../theme";
+import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
+import {
+  BarChartOutlined,
+  CalendarTodayOutlined,
+  ContactsOutlined,
+  DashboardOutlined,
+  DonutLargeOutlined,
+  HelpOutlineOutlined,
+  MapOutlined,
+  MenuOutlined,
+  PeopleAltOutlined,
+  PersonOutlined,
+  ReceiptOutlined,
+  TimelineOutlined,
+  WavesOutlined,
+} from "@mui/icons-material";
+import avatar from "../../../assets/images/avatar.png";
+import logo from "../../../assets/images/logo.png";
+import Item from "./Item";
+import { ToggledContext } from "../../../App";
+
+const SideBar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const { toggled, setToggled } = useContext(ToggledContext);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Sidebar
+      backgroundColor={colors.primary[400]}
+      rootStyles={{
+        border: 0,
+        height: "100%",
+      }}
+      collapsed={collapsed}
+      onBackdropClick={() => setToggled(false)}
+      toggled={toggled}
+      breakPoint="md"
+    >
+      <Menu
+        menuItemStyles={{
+          button: { ":hover": { background: "transparent" } },
+        }}
+      >
+        <MenuItem
+          rootStyles={{
+            margin: "10px 0 20px 0",
+            color: colors.gray[100],
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            {!collapsed && (
+              <Box
+                display="flex"
+                alignItems="center"
+                gap="12px"
+                sx={{ transition: ".3s ease" }}
+              >
+                <Typography
+                  variant="h4"
+                  fontWeight="bold"
+                  textTransform="capitalize"
+                  color={colors.greenAccent[500]}
+                >
+                  Havenlifestyles
+                </Typography>
+              </Box>
+            )}
+            <IconButton onClick={() => setCollapsed(!collapsed)}>
+              <MenuOutlined />
+            </IconButton>
+          </Box>
+        </MenuItem>
+      </Menu>
+      {!collapsed && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "10px",
+            mb: "25px",
+          }}
+        >
+          <Avatar
+            alt="avatar"
+            src={avatar}
+            sx={{ width: "100px", height: "100px" }}
+          />
+          <Box sx={{ textAlign: "center" }}>
+            <Typography variant="h3" fontWeight="bold" color={colors.gray[100]}>
+              Tony Stark
+            </Typography>
+            <Typography
+              variant="h6"
+              fontWeight="500"
+              color={colors.greenAccent[500]}
+            >
+              VP Fancy Admin
+            </Typography>
+          </Box>
+        </Box>
+      )}
+
+      <Box mb={5} pl={collapsed ? undefined : "5%"}>
+        <Menu
+          menuItemStyles={{
+            button: {
+              ":hover": {
+                color: "#868dfb",
+                background: "transparent",
+                transition: ".4s ease",
+              },
+            },
+          }}
+        >
+          <Item
+            title="Dashboard"
+            path="/"
+            colors={colors}
+            icon={<DashboardOutlined />}
+          />
+        </Menu>
+        {/* <Typography
+          variant="h6"
+          color={colors.gray[300]}
+          sx={{ m: "15px 0 5px 20px" }}
+        >
+          {!collapsed ? "Data" : " "}
+        </Typography>{" "} */}
+        <Menu
+          menuItemStyles={{
+            button: {
+              ":hover": {
+                color: "#868dfb",
+                background: "transparent",
+                transition: ".4s ease",
+              },
+            },
+          }}
+        >
+          <Item
+            title="Users"
+            path="/team"
+            colors={colors}
+            icon={<PeopleAltOutlined />}
+          />
+          <Item
+            title="Posts"
+            path="/posts"
+            colors={colors}
+            icon={<ContactsOutlined />}
+          />
+          <Item
+            title="Pages"
+            path="/pages"
+            colors={colors}
+            icon={<ReceiptOutlined />}
+          />
+          <Item
+            title="Articles"
+            path="/articles"
+            colors={colors}
+            icon={<ReceiptOutlined />}
+          />
+        </Menu>
+        {/* <Typography
+          variant="h6"
+          color={colors.gray[300]}
+          sx={{ m: "15px 0 5px 20px" }}
+        >
+          {!collapsed ? "Pages" : " "}
+        </Typography> */}
+        <Menu
+          menuItemStyles={{
+            button: {
+              ":hover": {
+                color: "#868dfb",
+                background: "transparent",
+                transition: ".4s ease",
+              },
+            },
+          }}
+        >
+          <Item
+            title="Hero Carousel"
+            path="/herocarousel"
+            colors={colors}
+            icon={<PersonOutlined />}
+          />
+          <Item
+            title="Experts"
+            path="/experts"
+            colors={colors}
+            icon={<CalendarTodayOutlined />}
+          />
+          <Item
+            title="Settings"
+            path="/settings"
+            colors={colors}
+            icon={<HelpOutlineOutlined />}
+          />
+          <Item
+            title="Comments"
+            path="/comments"
+            colors={colors}
+            icon={<HelpOutlineOutlined />}
+          />
+          <Item
+            title="Testimonials"
+            path="/testimonials"
+            colors={colors}
+            icon={<HelpOutlineOutlined />}
+          />
+        </Menu>
+        {/* <Typography
+          variant="h6"
+          color={colors.gray[300]}
+          sx={{ m: "15px 0 5px 20px" }}
+        >
+          {!collapsed ? "Charts" : " "}
+        </Typography> */}
+        <Menu
+          menuItemStyles={{
+            button: {
+              ":hover": {
+                color: "#868dfb",
+                background: "transparent",
+                transition: ".4s ease",
+              },
+            },
+          }}
+        >
+          <Item
+            title="Mail SMTP"
+            path="/mailsmtp"
+            colors={colors}
+            icon={<BarChartOutlined />}
+          />
+        </Menu>
+      </Box>
+    </Sidebar>
+  );
+};
+
+export default SideBar;
